a0d2546ac1469564b89a0646f0e6fac4449ef3f8
fatalfeel <fatalfeel@hotmail.com>
2014-09-23 13:41:05 +0800

    change order

--- RT3DApp/source/GUI/StencilMenu.cpp
+++ RT3DApp/source/GUI/StencilMenu.cpp
@@ -85,35 +85,6 @@ void StencilInitScene()
 	s_node->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
 	s_node->setMaterialTexture(0, driver->getTexture((GetBaseAppPath()+"game/particlewhite.bmp").c_str()));
 
-		// create a particle system
-
-	scene::IParticleSystemSceneNode* ps = smgr->addParticleSystemSceneNode(false);
-
-	scene::IParticleEmitter* em = 
-	ps->createBoxEmitter(	core::aabbox3d<f32>(-7,0,-7,7,1,7), // emitter size
-							core::vector3df(0.0f,0.06f,0.0f),   // initial direction
-							80,100,                             // emit rate
-							video::SColor(0,255,255,255),       // darkest color
-							video::SColor(0,255,255,255),       // brightest color
-							800,2000,0,                         // min and max age, angle
-							core::dimension2df(10.f,10.f),         // min size
-							core::dimension2df(20.f,20.f)  );        // max size
-		
-
-	ps->setEmitter(em); // this grabs the emitter
-	em->drop(); // so we can drop it here without deleting it
-
-	scene::IParticleAffector* paf = ps->createFadeOutParticleAffector();
-	ps->addAffector(paf); // same goes for the affector
-	paf->drop();
-
-	ps->setPosition(core::vector3df(-70,60,40));
-	ps->setScale(core::vector3df(2,2,2));
-	ps->setMaterialFlag(video::EMF_LIGHTING, false);
-	ps->setMaterialFlag(video::EMF_ZWRITE_ENABLE, false);
-	ps->setMaterialTexture(0, driver->getTexture((GetBaseAppPath()+"game/fire.bmp").c_str()));
-	ps->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
-
 	/*
 	Next we add a volumetric light s_node, which adds a glowing fake area light to
 	the scene. Like with the billboards and particle systems we also assign a
@@ -156,8 +127,36 @@ void StencilInitScene()
 	}
 
 	
-	// add animated character
+	// create a particle system
+	scene::IParticleSystemSceneNode* ps = smgr->addParticleSystemSceneNode(false);
 
+	scene::IParticleEmitter* em = 
+	ps->createBoxEmitter(	core::aabbox3d<f32>(-7,0,-7,7,1,7), // emitter size
+							core::vector3df(0.0f,0.06f,0.0f),   // initial direction
+							80,100,                             // emit rate
+							video::SColor(0,255,255,255),       // darkest color
+							video::SColor(0,255,255,255),       // brightest color
+							800,2000,0,                         // min and max age, angle
+							core::dimension2df(10.f,10.f),         // min size
+							core::dimension2df(20.f,20.f)  );        // max size
+		
+
+	ps->setEmitter(em); // this grabs the emitter
+	em->drop(); // so we can drop it here without deleting it
+
+	scene::IParticleAffector* paf = ps->createFadeOutParticleAffector();
+	ps->addAffector(paf); // same goes for the affector
+	paf->drop();
+
+	ps->setPosition(core::vector3df(-70,60,40));
+	ps->setScale(core::vector3df(2,2,2));
+	ps->setMaterialFlag(video::EMF_LIGHTING, false);
+	ps->setMaterialFlag(video::EMF_ZWRITE_ENABLE, false);
+	ps->setMaterialTexture(0, driver->getTexture((GetBaseAppPath()+"game/fire.bmp").c_str()));
+	ps->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+
+		
+	// add animated character
 	mesh = smgr->getMesh((GetBaseAppPath()+"game/dwarf.x").c_str());
 
 	a_node = smgr->addAnimatedMeshSceneNode(mesh);
